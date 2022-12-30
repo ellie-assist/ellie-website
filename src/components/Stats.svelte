ae2a4d98bba7f0d04d27b135922bc6916b8ea01e
James Ivings <james@squarecat.io>
2022-12-30 12:42:38 +0700

    initial commit

---
+++ src/components/Stats.svelte
@@ -0,0 +1,26 @@
+<script>
+  const apiUrl = window.location.href.includes("localhost")
+    ? "http://localhost:3113"
+    : "https://api.ellieai.com";
+
+  let opacity = 0;
+  let totalUsers = 0;
+  let repliesGenerated = 0;
+
+  async function getStats() {
+    const result = await (await fetch(`${apiUrl}/extension/stats`)).json();
+    const { usage } = result;
+    totalUsers = result.totalUsers;
+    repliesGenerated = usage.totalRepliesGenerated;
+    opacity = 1;
+  }
+  getStats();
+</script>
+
+<div id="stats" class="stats" style:opacity>
+  <p>
+    Join <strong id="total-users">{totalUsers}</strong> users, who have used
+    Ellie to generate over
+    <strong id="replies-generated">{repliesGenerated}</strong> email replies!
+  </p>
+</div>
