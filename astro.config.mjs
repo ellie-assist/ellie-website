6da4a751655731ffe8b372ac0c72edc852ef2894
James Ivings <james@squarecat.io>
2023-01-04 15:49:50 +0700

    added sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import astroI18next from "astro-i18next";
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://ellieai.com",
-  integrations: [svelte(), astroI18next()],
+  integrations: [svelte(), astroI18next(), sitemap()],
 });
