18d5e5e69fb369b3db9675bad75c01615894045e
James Ivings <james@squarecat.io>
2023-04-23 10:31:21 +0200

    add new section for advanced training

---
+++ scripts/translate.js
@@ -0,0 +1,7 @@
+const prompt = `Translate this into German, Spanish, French, Russian, and Mandarin: 
+
+"title": "How does teaching Ellie new information work?",
+"content": [
+    "You can provide Ellie with documents or URLs. Ellie will read the documents and crawl the URLs to <em>learn</em> the information.",
+    "If Ellie thinks that some of the information that it has learned is relevant to the email it's currently replying to, then it will intelligently include that information in it's reply."
+]`;
