8bf862c48529433db2c8f4872bef1ef4c085a85d
James Ivings <james@squarecat.io>
2024-03-11 19:14:33 -0400

    upgraded astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://tryellie.com",
-  integrations: [svelte(), astroI18next(), sitemap()],
+  integrations: [svelte(), astroI18next(), sitemap()]
 });
